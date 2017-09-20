41e733567a2c12f5716dd50c588d7de97d3e055b
Florin Lazau <florin.lazau@lateral-inc.com>
2017-09-20 15:46:59 +0300

    - add new files

--- SamplePackage/build.ps1
+++ SamplePackage/build.ps1
@@ -1,14 +1,9 @@
 ﻿if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
-EnsurePsbuildInstalled
 
-exec { & dotnet restore }
-
-Invoke-MSBuild
+dotnet build 
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet test .\test\SamplePackage -c Release }
-
-exec { & dotnet pack .\src\SamplePackage -c Release -o .\artifacts --version-suffix=$revision }  
+dotnet pack  -c Debug -o .\artifacts
