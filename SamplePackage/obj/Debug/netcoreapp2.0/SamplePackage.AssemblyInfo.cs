bcd1149688e6acb92ed13da663d2f515371b4d96
Florin Lazau <florin.lazau@lateral-inc.com>
2017-09-21 11:44:23 +0300

    - min ch

--- SamplePackage/obj/Debug/netcoreapp2.0/SamplePackage.AssemblyInfo.cs
+++ SamplePackage/obj/Debug/netcoreapp2.0/SamplePackage.AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lateral INC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description from florin")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SamplePackage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SamplePackage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
